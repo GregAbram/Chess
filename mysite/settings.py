5de966b9a085e2e2d3f5204222cb5f820d65dc1b
Greg Abram <gda@tacc.utexas.edu>
2026-03-15 12:31:13 -0500

    Add logging configuration to settings.py
    Add logging configuration to settings.

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,3 +105,23 @@ if not DEBUG:
     SECURE_SSL_REDIRECT = True
     
 print("XXXXXXXXXXXXXXXXX DATABASE_URL =", os.environ.get("DATABASE_URL"))
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
