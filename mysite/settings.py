a955a8dfa66404cd53c8b453801b5cd6196fcd84
Greg Abram <gda@tacc.utexas.edu>
2026-03-15 11:43:32 -0500

    Refactor secret key handling in settings.py
    Updated secret key retrieval to use environment variable.

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hsuapl_q7rceqf63i!pmm*%c(8h72-cf(jmc(1d32!zz(2q+*k'
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-only-not-safe")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
