574567cdca78c77a6c7964f700f54c9d9b245655
Greg Abram <gda@tacc.utexas.edu>
2026-03-15 13:07:43 -0500

    fix

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit  # stop on errors
-
+echo AAAAAAA build.sh
 pip install -r requirements.txt
+echo BBBBBBB build.sh
 python manage.py collectstatic --noinput
+echo CCCCCCC build.sh
 python manage.py migrate
+echo DDDDDDD build.sh
